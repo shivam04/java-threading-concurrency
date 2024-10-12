9928d93cf434873ef6737cd45ebc86ef5f11b944
Shiv@m Sinh@ <sinhashivam04@gmail.com>
2024-10-13 00:06:59 +0530

    fix code

--- src/threading/atomioperations/atomicintegers/Main.java
+++ src/threading/atomioperations/atomicintegers/Main.java
@@ -4,21 +4,22 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
+
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        incrementingThread.start();
+        decrementingThread.start();
+
+        incrementingThread.join();
+        decrementingThread.join();
+
+        System.out.println("We currently have " + inventoryCounter.getItems() + " items");
+    }
+
     public static class DecrementingThread extends Thread {
-
-        public static void main(String[] args) throws InterruptedException {
-            InventoryCounter inventoryCounter = new InventoryCounter();
-            IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
-            DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
-
-            incrementingThread.start();
-            decrementingThread.start();
-
-            incrementingThread.join();
-            decrementingThread.join();
-
-            System.out.println("We currently have " + inventoryCounter.getItems() + " items");
-        }
 
         private InventoryCounter inventoryCounter;
 
